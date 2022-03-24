3af23d9bb50c45da67d037f7938791fb8d3cdc2e
daeaevaeaed <fischertechniklol@yahoo.com>
2022-03-24 16:22:57 +0100

    queue start, edit stack gen

--- 02_verkettete_liste/queue_gen.py
+++ 02_verkettete_liste/queue_gen.py
@@ -13,7 +13,7 @@ from abc import ABC, abstractmethod
 
 T = TypeVar("T")
 
-class Stacknode(Generic[T]):
+class Queuenode(Generic[T]):
     """Ein Listen.
     
     Attributes:
@@ -21,22 +21,22 @@ class Stacknode(Generic[T]):
         next_node: Der Nachfolger des Listenknotens.
     """
     data: T
-    next_node: Optional['Stacknode[T]']
+    next_node: Optional['Queuenode[T]']
     
-    def __init__(self, data: T, next_node: Optional['Stacknode[T]'] = None):
+    def __init__(self, data: T, next_node: Optional['Queuenode[T]'] = None):
         self.data = data
         self.next_node = next_node
         
     def __str__(self) -> str:
         return str(self.data)
         
-class Stack(Collection[T]):
+class Queue(Collection[T]):
     """Abstrakte Liste
 
     Attributes:
         first_node: Der erste Knoten der Liste
     """
-    first_node: Optional[Stacknode]
+    first_node: Optional[Queuenode]
     
     def __bool__(self) -> bool:
         """Gibt zurück, ob die Liste leer ist.
@@ -45,6 +45,53 @@ class Stack(Collection[T]):
             True, falls die Liste leer ist; False sonst
         """
         return self.first_node == None
+    
+    # @abstractmethod         
+    # def __getitem__(self, index: int) -> T:
+    #     """Gibt den n-ten Wert (0-basierte Zählweise) der Liste zurück.
+        
+    #     Attr:
+    #         index: Der Index des gewünschten Elements
+    #     Returns:
+    #         Den n-ten Wert
+    #     Raises:
+    #         KeyError: Index not found
+    #     """
+    #     pass
+    
+    # @abstractmethod
+    # def __setitem__(self, index: int, value: T) -> None:
+    #     """Ändert den Wert an der gegebenen Stelle (0-basierte Zählweise) der Liste.
+
+    #     Attr:
+    #         index: Der Index des zu ändernden Elements
+    #         value: Der neue Wert
+    #     Raises:
+    #         KeyError: Index not found
+    #     """
+    #     pass
+    
+    # @abstractmethod
+    # def __delitem__(self, index: int) -> None:
+    #     """ Entfernt das Element, das an der angegeben Stelle steht, aus der Liste.
+        
+    #     Attr:
+    #         index: Die Stelle, von der der Wert entfernt werden soll
+    #     Raises:
+    #         KeyError: Index not found
+    #     """
+    #     pass
+    
+    # @abstractmethod
+    # def __contains__(self, value: object) -> bool:
+    #     """Gibt zurück, ob ein Wert sich in der Liste befindet
+
+    #     Atrr:
+    #         value: Der zu suchende Wert
+    #     Returns:
+    #         True, falls der Wert enthalten ist; False sonst
+    #     """
+    #     pass
 
     def __iter__(self) -> Iterator[T]:
         """Iterator über die Daten"""
@@ -53,34 +100,50 @@ class Stack(Collection[T]):
             yield node.data
             node = node.next_node
  
+    # @abstractmethod
+    # def __len__(self) -> int:
+    #     """Gibt die Anzahl der Elemente der Liste zurück."""
+    #     pass
+    
+    # @abstractmethod
+    # def append(self, value: T) -> None:
+    #     """Hängt einen neuen Wert hinten an die Liste an.
+
+    #     Attr:
+    #         value: Der anzuhängende Wert
+    #     """
+    #     pass 
+    
+    # @abstractmethod
+    # def insert(self, index: int, value: T) -> None:
+    #     """Fügt einen neuen Wert an einer gewünschten Stelle in die Liste ein.
+        
+    #     Attr:
+    #         index: Die Stelle, an der der neue Wert stehen soll (0 <= index <= len)
+    #         value: Der einzufügende Wert
+    #     """
+    #     pass
+        
+    # @abstractmethod
+    # def remove(self, value: T) -> None:
+    #     """ Entfernt den ersten gefundenen Wert aus der Liste.
+        
+    #     Attr:
+    #         value: Wert der entfernt werden soll.
+    #     Raises:
+    #         ValueError: List.remove(value): value not in list
+    #     """
+    #     pass
+    
     def __str__(self) -> str:
         """Gibt die Listenelemente hintereinander aus."""
         return " ".join([str(e) for e in self])
         
-class Stackiterative(Stack[T]):
+class Queueiterative(Queue[T]):
     """Iterative Implementierung einer Verketteten Liste"""
     def __init__(self):
         self.first_node = None
     
-    def top(self) -> object:
-        if not self.isEmpty():
-            node = self.first_node
-            while node.next_node is not None:
-                node = node.next_node
-            return node.data
-    
-    def push(self, value: object) -> None:
-        if not self.first_node:
-            self.first_node = Stacknode(value, None)
-            return
-        prev_node = None
-        next_node = self.first_node
-        while next_node is not None:
-            prev_node = next_node
-            next_node = next_node.next_node
-        prev_node.next_node = Stacknode(value, None)
-    
-        
         
     def __getitem__(self, index: int) -> T:
         if index < 0:
@@ -133,100 +196,44 @@ class Stackiterative(Stack[T]):
         return False
     
     def __len__(self) -> int:
-        length = 0
-        node = self.first_node
-        while node is not None:
-            node = node.next_node
-            length += 1
-        return length
+        pass
         
     def remove(self, value: T) -> None:
-        node = self.first_node
-        if node is not None and node.data == value:
-            self.first_node = node.next_node      
-        previous_node = node
-        while node is not None:
-            if node.data == value and previous_node is not None:
-                
-                previous_node.next_node = node.next_node
-#                del node
-                return
-            previous_node = node
-            node = node.next_node
-        raise ValueError("List.remove(value): value not in list")
+        pass
 
     def pop(self):
-        print(self.isEmpty())
-        if self.isEmpty():
-            return
-            print("error")
-        data = self.top()
-        self.remove()
-        return data
+        pass
     
     def isEmpty(self):
-        node = self.first_node
-        for i in self:
-            if node.data == None or node == None or node.next_node == None:
-                # print("empty")
-                return True
-            else:
-                # print("full")
-                return False
+        pass
     
     def __len__(self):
-        node = self.first_node
-        length = 1
-        # print("Länge")
-        while node is not None:
-            length += 1
-            node = node.next_node
-        return length + 2
+        pass
     
     def clear(self):
-        while(not self.isEmpty()):
-            self.remove()
-        self.first_node.data = None
-        # self.first_node = None
+        pass
                 
     def remove(self):
-        if not self.isEmpty:
-            node = self.first_node
-            prev_node = None
-            if self.first_node.next_node is not None:
-                while node.next_node is not None:
-                    prev_node = node
-                    node = node.next_node
-                prev_node.next_node = None
+        pass
             
     def __str__(self) -> str:
-        if not self.isEmpty():
-            data = ""
-            node = self.first_node
-            for i in self:
-                data  += str(node.data) + " "
-                node = node.next_node
-            return data
-                
-                
+        pass
+        
+    def enqueue(self, value: object) -> None:
+        if not self.first_node:
+            self.first_node = Queuenode(value, None)
+            return
+        prev_node = None
+        next_node = self.first_node
+        while next_node is not None:
+            prev_node = next_node
+            next_node = next_node.next_node
+        prev_node.next_node = Queuenode(value, None)
     
-s = Stackiterative()
-s.pop()
-s.push(10)
-s.push(11)
-s.push(12)
-s.push(13)
-s.push(14)
-s.push(15)
-# print("hi")
-# print(stack.__str__())
-# print(stack.top())
-# print(stack.top())
-# stack.remove()
-# print(stack.top())
-# print(stack.__str__())
-# stack.clear()
-# print(s.__str__())
-s.pop()
-s.clear()
-# print(s.__str__())
+    def front(self):
+        return self.first_node.data
+        
+q = Queueiterative()
+q.enqueue(19)
+q.enqueue(20)
+print(q.front())
