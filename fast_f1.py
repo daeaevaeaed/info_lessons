370d0b89c47364d03a65304e6b25953ae859d7f6
daeaevaeaed <fischertechniklol@yahoo.com>
2022-03-31 15:30:24 +0200

    new plot trainings data; fastf1 qualiyfing of saudi

--- fast_f1.py
+++ fast_f1.py
@@ -1,5 +1,26 @@
 import fastf1
-fastf1.Cache.enable_cache('F:\cache')
-session = fastf1.get_session(2022, 1, "R")
+import random as rnd
+import matplotlib.pyplot as plt
+fastf1.Cache.enable_cache("A:\cache")
+session = fastf1.get_session(2022, 2, "Q")
 session.load()
-print(session.laps)
+# driver_number = []
+# position = []
+# for i in session.results.Q3:
+#     driver_number.append[]
+a = []
+number = 1
+for index in session.results.FullName:
+    print(index)
+    a.append(number)
+plot = plt.plot(a, session.results.Abbreviation, "or")
+plot
+plt.xlabel(None)
+plt.ylabel(None)
+plt.tight_layout()
+plt.xscale = 1
+plt.grid(True)
+plt.colormaps()
+plt.setp(plot, 'color', 'g', 'linewidth', 1.0)
+plt.show()
+print(session.results.columns)
---
+++ plot.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import random as rnd
+plt.xlabel("X-Achse")
+plt.ylabel("Y-Achse")
+plt.plot([1, 2, 3], [3,2,1], "ro")
+plt.show()
